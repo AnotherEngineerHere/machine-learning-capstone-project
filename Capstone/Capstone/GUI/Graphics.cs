87c2d26813d9297c7908d7ef5da56a636b82bc15
Josue Rodriguez Pineda <rojopi@hotmail.es>
2020-10-30 14:04:59 -0500

    Clear methods of graphics

--- Capstone/Capstone/GUI/Graphics.cs
+++ Capstone/Capstone/GUI/Graphics.cs
@@ -26,13 +26,7 @@ namespace Capstone.GUI
 
         public void CreateChart() {
             noShow();
-            string[] brands = {"Renault", "Chevrolet", "Ford", "Gm", "Audi", "Fiat", "Kia", "Nissan", "Subaru", "Peugeot" };
-            int[] brandsC = {151, 167, 170, 184,181, 175,193,181,158,168};
-            chartMarca.Series.Clear();
-            chartMarca.Series.Add("carros por marca");
-            for (int i = 0; i<brands.Length; i++) {
-                chartMarca.Series["carros por marca"].Points.AddXY(brands[i], brandsC[i]);
-            }
+            
             chartMarca.Visible = true;
         
         }
@@ -40,14 +34,7 @@ namespace Capstone.GUI
         private void butPastelPor_Click(object sender, EventArgs e)
         {
             noShow();
-            string[] brands = { "Renault", "Chevrolet", "Ford", "Gm", "Audi", "Fiat", "Kia", "Nissan", "Subaru", "Peugeot" };
-            int[] brandsC = { 151, 167, 170, 184, 181, 175, 193, 181, 158, 168 };
-            porcentaje.Series["Series1"].Points.Clear();
-            for (int i = 0; i < brands.Length; i++)
-            {
-                double d = brandsC[i];
-                porcentaje.Series["Series1"].Points.AddXY(brands[i],  d);
-            }
+           
             porcentaje.Visible = true;
             
         }
@@ -56,28 +43,14 @@ namespace Capstone.GUI
         private void precioMaximo_Click(object sender, EventArgs e)
         {
             noShow();
-            string[] brands = { "Very high", "high", "med", "low"};
-            int[] brandsC = { 432, 432, 432, 432,432};
-            chartMarca.Series.Clear();
-            chartMarca.Series.Add("carros por precio");
-            for (int i = 0; i < brands.Length; i++)
-            {
-                chartMarca.Series["carros por precio"].Points.AddXY(brands[i], brandsC[i]);
-            }
+           
             chartMarca.Visible = true;
         }
 
         private void buttonPorCaros_Click(object sender, EventArgs e)
         {
             noShow();
-            string[] brands = { "Very high", "high", "med", "low" };
-            int[] brandsC = { 1,1,1,1};
-            porcentaje.Series["Series1"].Points.Clear();
-            for (int i = 0; i < brands.Length; i++)
-            {
-                double d = brandsC[i];
-                porcentaje.Series["Series1"].Points.AddXY(brands[i], d);
-            }
+            
             porcentaje.Visible = true;
 
         }
@@ -85,23 +58,9 @@ namespace Capstone.GUI
         private void buttPuertas_Click(object sender, EventArgs e)
         {
             noShow();
-            string[] brands = { "dos", "tres", "cuatro", "cinco"};
-            int[] brandsC = { 432,432,432,432 };
-            chartMarca.Series.Clear();
-            chartMarca.Series.Add("carros por marca");
-            for (int i = 0; i < brands.Length; i++)
-            {
-                chartMarca.Series["carros por marca"].Points.AddXY(brands[i], brandsC[i]);
-            }
+            
             chartMarca.Visible = true;
-            /*puertas.Series.Add("puertas");
-            puertas.Series["puertas"].Points.Clear();
-            for (int i = 0; i < brands.Length; i++)
-            {
-                puertas.Series["puertas"].Points.AddXY(brands[i], brandsC[i]);
-            }
-            puertas.Visible = true;
-            */
+            
         }
 
         public void noShow() {
