9c71b0f1477c95cba77338e68061da845538cfe7
Andres Orozco <juanxxi2015@gmail.com>
2020-11-15 16:24:57 -0500

    Litte bug found and class TreeNuget partially done
    Little bug found and the class is not working yet

--- Capstone/Capstone/Decision_TreeNuget/TreeNuget.cs
+++ Capstone/Capstone/Decision_TreeNuget/TreeNuget.cs
@@ -15,6 +15,30 @@ namespace Capstone.Decision_TreeNuget
 {
     public class TreeNuget
     {
+        public const string NUGET = @"../../Data/nuget.csv";
 
+        public const string PROBE = @"../../Data/probe.csv";
+
+        private string path;
+
+        private DataTable data;
+
+        private DataTable pb;
+
+        private CSVFileHandler csv;
+
+        private DecisionTree tree;
+
+        private string result;
+
+        private Codification codebook;
+
+        public TreeNuget(string path)
+        {
+            this.path = path;
+            csv = new CSVFileHandler();
+            CreateTree();
+            result = "";
+        }
     }
 }
