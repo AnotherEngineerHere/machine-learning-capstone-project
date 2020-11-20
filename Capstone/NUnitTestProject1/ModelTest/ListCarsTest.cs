e1f65ba03f48e65b3b37736c36b9b88c5e35896b
Josue Rodriguez Pineda <rojopi@hotmail.es>
2020-11-20 15:19:19 -0500

    range test

--- Capstone/NUnitTestProject1/ModelTest/ListCarsTest.cs
+++ Capstone/NUnitTestProject1/ModelTest/ListCarsTest.cs
@@ -20,6 +20,21 @@ namespace NUnitTestProject1
             
         }
 
-        
+        public void rangeTest()
+        {
+            Car c1 = new Car("vhigh", "vhigh", "2", "2", "med", "med", "bad");
+            Car c2 = new Car("vhigh", "vhigh", "3", "2", "med", "med", "bad");
+            Car c3 = new Car("vhigh", "vhigh", "4", "2", "med", "med", "bad");
+            Car c4 = new Car("vhigh", "vhigh", "3", "2", "med", "med", "bad");
+            ListCars lc = new ListCars();
+            lc.Add_Car("vhigh", "vhigh", "2", "2", "med", "med", "bad");
+            lc.Add_Car("vhigh", "vhigh", "3", "2", "med", "med", "bad");
+            lc.Add_Car("vhigh", "vhigh", "4", "2", "med", "med", "bad");
+            lc.Add_Car("vhigh", "vhigh", "3", "2", "med", "med", "bad");
+            Assert.AreEqual(c1.CarF, lc.Get_Range_From_Values);
+
+        }
+
+
     }
 }
